785d88cb5884ae4f9575e59f37b9b851de2a5978
TANGhz17 <15851238039@163.com>
2020-09-28 23:33:30 +0800

    0.0.1

--- src/cn/tanghz17/calculator/Main.java
+++ src/cn/tanghz17/calculator/Main.java
@@ -1,36 +1,17 @@
 package cn.tanghz17.calculator;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
-    private static void calculatorGUI(){
-        //JFrame.setDefaultLookAndFeelDecorated(true);
-        //创建窗口frame
-        JFrame frame = new JFrame("TANGhz17计算器");
-        //窗口大小
-        frame.setSize(350, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JButton zero = new JButton("0");
-        frame.getContentPane().add(zero);
-
-        JButton one = new JButton("1");
-        frame.getContentPane().add(one);
-
-        JButton two = new JButton("2");
-        frame.getContentPane().add(two);
-
-        //显示窗口
-        frame.pack();
-        frame.setVisible(true);
-    }
     public static void main(String[] args) {
-	// write your code here
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                calculatorGUI();
-            }
-        });
+	    JFrame jf=new JFrame("TANGhz17计算器");
+	    jf.setBounds(300,100,400,200);
+	    JPanel jp=new JPanel();
+	    JLabel jl=new JLabel("TANGhz17计算器");
+	    jp.setBackground(Color.white);
+	    jp.add(jl);
+	    jf.add(jp);
+	    jf.setVisible(true);
     }
 }
