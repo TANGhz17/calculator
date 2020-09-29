ea6842d5b38e9bc0c882f67b3a0f37a3a6e6a6b2
TANGhz17 <15851238039@163.com>
2020-09-30 00:45:17 +0800

    0.0.5

--- src/cn/tanghz17/calculator/GridLayoutPosition.java
+++ src/cn/tanghz17/calculator/GridLayoutPosition.java
@@ -2,9 +2,12 @@ package cn.tanghz17.calculator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class GridLayoutPosition extends JFrame {
-    public GridLayoutPosition(String Frametitle){
+    JTextField inputField =new  JTextField("");
+
+    public GridLayoutPosition(String frameTitle){
         GridBagLayout gbaglayout=new GridBagLayout();           //创建GridBagLayout布局管理器
         GridBagConstraints constraints=new GridBagConstraints();
 
@@ -12,7 +15,8 @@ public class GridLayoutPosition extends JFrame {
         constraints.weightx=0.0;                                //恢复默认值
         constraints.gridwidth = GridBagConstraints.REMAINDER;   //结束行
 
-        JTextField inputField = new JTextField("");
+        inputField.setEditable(false);
+        inputField.setFont(new Font("黑体",Font.BOLD,16));
         gbaglayout.setConstraints(inputField, constraints);
         add(inputField);
         constraints.weightx=0.5;    // 指定组件的分配区域
@@ -46,16 +50,27 @@ public class GridLayoutPosition extends JFrame {
         constraints.gridwidth=GridBagConstraints.REMAINDER;
         makeButton("+",gbaglayout,constraints);
         constraints.gridwidth=1;
-        
+
         setLayout(gbaglayout);                  //使用GridBagLayout布局管理器
         setSize(400,200);
-        setTitle(Frametitle);
+        setTitle(frameTitle);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     public void makeButton(String title, GridBagLayout gridBagLayout, GridBagConstraints constraints){
         JButton button=new JButton(title);    //创建Button对象
+        button.setFont(new Font("黑体",Font.BOLD,16));    //修改字体
         gridBagLayout.setConstraints(button,constraints);
+        button.addActionListener(new AbstractAction() {         //按钮点击监控触发事件
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputFieldText = inputField.getText();
+                inputField.setText(inputFieldText+title);       //改变inputField内容
+            }
+        });
         add(button);
+    }
+    public void clickButton(){
+
     }
 }
